f491a56dd693ece82ec4601d59450a7a3369a6c9
深搜暴力斌 <3197235088@qq.com>
2018-03-06 22:58:27 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+HZAU TEST OJ SYSTEM
+需要安装的包
+python3
+flask
+flask_script
--- app/index.py
+++ app/index.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
 from flask_script import Manager
 
 app = Flask(__name__)
